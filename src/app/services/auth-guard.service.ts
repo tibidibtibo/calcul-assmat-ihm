4bccf628babed44936b7ba98353a4031bff93245
tdeboissieu <tdeboissieu@ose.dev.fr>
2018-11-07 18:29:10 +0100

    [Security] : auth

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,12 +1,15 @@
+import { TokenStorageService } from './token.storage.service';
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  constructor(public auth: AuthService, public router: Router) {}
+
+  constructor(public tokenService: TokenStorageService, public router: Router) {}
+
   canActivate(): boolean {
-    if (!this.auth.authenticated) {
+    if (!this.tokenService.getToken()) {
       this.router.navigate(['login']);
       return false;
     }
