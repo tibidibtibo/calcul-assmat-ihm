7060767b1a6de99ca76c532fe406278f87985cc4
Thibault <thibault@de-boissieu.fr>
2018-12-21 12:15:05 +0100

    [Saisie] : modal delete

--- src/app/modules/saisie.module/gestion-saisie.component/gestion-saisie.component.ts
+++ src/app/modules/saisie.module/gestion-saisie.component/gestion-saisie.component.ts
@@ -3,9 +3,10 @@ import { Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { ReferentielService } from './../../../services/referentiel.service';
-import { Component } from "@angular/core";
+import { Component, TemplateRef } from "@angular/core";
 import { HttpService } from "../../../services/http.service";
 import { forkJoin } from 'rxjs/observable/forkJoin';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 
 @Component({
   selector: "gestion-saisie",
@@ -15,13 +16,15 @@ import { forkJoin } from 'rxjs/observable/forkJoin';
 export class GestionSaisieComponent {
 
   constructor(private httpService: HttpService, private refService: ReferentielService,
-    private fb: FormBuilder,private constantes: ConstService) {
+    private fb: FormBuilder,private constantes: ConstService, private modalService: BsModalService) {
     this.initListeSaisie(this.monthSelected, this.yearSelected);
     this.createForm();
   }
 
   public form: FormGroup;
+  public modalRef: BsModalRef;
 
+  public toDelete;
   public loading: boolean = false;
   public monthsList = this.constantes.MONTHS_LIST;
   public monthSelected = ((new Date()).getMonth() + 1).toString().padStart(2, "0");
@@ -54,13 +57,43 @@ export class GestionSaisieComponent {
     this.initListeSaisie(this.monthSelected, this.yearSelected);
   }
 
-  public deleteSaisie(saisie) {
-    this.httpService.supprimerSaisie(saisie.id).subscribe(ok => {
-      this.initListeSaisie(this.monthSelected, this.yearSelected);
-    })
+  public deleteSaisie(saisie, template: TemplateRef<any>) {
+    this.toDelete = {
+      id: saisie.id,
+      nom: saisie.refEnfant.nom + " / " + saisie.refEmploye.nom,
+      date: saisie.dateSaisie,
+      deleteFunction: this.deleteSaisieCall,
+      deleteEnCours: false
+    };
+    this.openDeleteModal(template);
+  }
+
+  public deleteSaisieCall(saisieId, httpService) {
+    return httpService.supprimerSaisie(saisieId);
   }
 
   public editSaisie(saisie) {
     // TODO
+  }
+
+  private openDeleteModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  public confirmDeletion(deleteFunction, paramId) {
+    this.toDelete.deleteEnCours = true;
+    deleteFunction(paramId, this.httpService).subscribe(
+        ok => {
+          this.initListeSaisie(this.monthSelected, this.yearSelected);
+          this.toDelete.deleteEnCours = false;
+          this.modalRef.hide();
+        }, ko => {
+          this.toDelete.deleteEnCours = false;
+        }
+    );
+  }
+
+  public declineDeletion() {
+    this.modalRef.hide();
   }
 }
